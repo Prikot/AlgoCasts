e24ed3d81518e0c69433134e67db32da266a0d92
Prikot <a.prikotov@gmail.com>
2019-02-19 16:07:02 +0300

    complete tasks

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,16 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    return str.split('').every((char, i) => char === str[str.length - i - 1]);
+}
 
 module.exports = palindrome;
+
+
+
+// function palindrome(str) {
+//     let x = str.split('').reduce((rev, char) => char + rev,'');
+//
+//     return str === x;
+// }
